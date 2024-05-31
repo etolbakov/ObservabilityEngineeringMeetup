ba2f0adfdf7d5c0e743e3e883470d41932e21010
Eugene Tolbakov <ev.tolbakov@gmail.com>
2024-05-31 11:35:51 +0100

    chore: more

--- opensearch/module/locals.tf
+++ opensearch/module/locals.tf
@@ -1,38 +1,31 @@
 locals {
-
   index_templates = merge({
     for filename in var.index_template_files :
-    replace(basename(filename), "/\\.(ya?ml|json)$/", "") =>
-    length(regexall("\\.ya?ml$", filename)) > 0 ? yamldecode(file(filename)) : jsondecode(file(filename))
+    replace(basename(filename), "/\\.yaml.tftpl$/", "") => yamldecode(file(filename))
   }, var.index_templates)
 
   index_patterns = merge({
-    for filename in var.index_patterns :
-    replace(basename(filename), "/\\.(ya?ml|json)$/", "") =>
-    length(regexall("\\.ya?ml$", filename)) > 0 ? yamldecode(file(filename)) : jsondecode(file(filename))
+    for filename in var.index_patterns_files :
+    replace(basename(filename), "/\\.yaml.tftpl$/", "") => yamldecode(file(filename))
   }, var.index_patterns)
 
   ism_policies = merge({
     for filename in var.ism_policy_files :
-    replace(basename(filename), "/\\.(ya?ml|json)$/", "") =>
-    length(regexall("\\.ya?ml$", filename)) > 0 ? yamldecode(file(filename)) : jsondecode(file(filename))
+    replace(basename(filename), "/\\.yaml.tftpl$/", "") => yamldecode(file(filename))
   }, var.ism_policies)
 
   ingest_pipelines = merge({
     for filename in var.ingest_pipelines :
-    replace(basename(filename), "/\\.(ya?ml|json)$/", "") =>
-    length(regexall("\\.ya?ml$", filename)) > 0 ? yamldecode(file(filename)) : jsondecode(file(filename))
+    replace(basename(filename), "/\\.yaml.tftpl$/", "") => yamldecode(file(filename))
   }, var.ingest_pipelines)
 
   roles = merge({
     for filename in var.role_files :
-    replace(basename(filename), "/\\.(ya?ml|json)$/", "") =>
-    length(regexall("\\.ya?ml$", filename)) > 0 ? yamldecode(file(filename)) : jsondecode(file(filename))
+    replace(basename(filename), "/\\.yaml.tftpl$/", "") => yamldecode(file(filename))
   }, var.roles)
 
   role_mappings = merge({
     for filename in var.role_mapping_files :
-    replace(basename(filename), "/\\.(ya?ml|json)$/", "") =>
-    length(regexall("\\.ya?ml$", filename)) > 0 ? yamldecode(file(filename)) : jsondecode(file(filename))
+    replace(basename(filename), "/\\.yaml.tftpl$/", "") => yamldecode(file(filename))
   }, var.role_mappings)
 }
